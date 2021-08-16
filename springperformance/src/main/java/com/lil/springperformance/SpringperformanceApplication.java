19feeb9abadfb4c47bba2430a082bec980fa16a3
kdflint <kathy.flint@northbridgetech.org>
2021-08-16 12:26:13 -0500

    preliminary project bed

---
+++ springperformance/src/main/java/com/lil/springperformance/SpringperformanceApplication.java
@@ -0,0 +1,13 @@
+package com.lil.springperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringperformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringperformanceApplication.class, args);
+	}
+
+}
