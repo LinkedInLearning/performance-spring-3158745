85d4e32a26ccb9c7771e675e2de507f9fd77be0b
kdflint <kathy.flint@northbridgetech.org>
2021-09-25 10:57:32 -0500

    2_2 lesson prep

--- demo-api/src/main/java/com/lil/springperformance/api/controller/DemoController.java
+++ demo-api/src/main/java/com/lil/springperformance/api/controller/DemoController.java
@@ -1,6 +1,9 @@
 package com.lil.springperformance.api.controller;
 
+import com.lil.springperformance.api.DemoApiApplication;
 import com.lil.springperformance.api.domain.GenericResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +13,8 @@ import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class DemoController {
+
+    private static Logger logger = LoggerFactory.getLogger(DemoApiApplication.class);
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
@@ -21,6 +26,7 @@ public class DemoController {
 
     @GetMapping("/hello-api")
     public GenericResponse sayHello(@RequestParam(name="name", required=false, defaultValue="Learner") String name) {
+        logger.debug("Debug message");
         return new GenericResponse(counter.incrementAndGet(), String.format(template, name));
     }
 
