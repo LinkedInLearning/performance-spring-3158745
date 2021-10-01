9f5409dc8ce62bdc9c5478930c3401ca60ce292a
kdflint <kathy.flint@northbridgetech.org>
2021-10-01 16:47:23 -0500

    jfr

--- demo-api/src/main/java/com/lil/springperformance/api/DemoApiApplication.java
+++ demo-api/src/main/java/com/lil/springperformance/api/DemoApiApplication.java
@@ -6,9 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.metrics.jfr.FlightRecorderApplicationStartup;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -19,8 +21,12 @@ public class DemoApiApplication {
 	public static void main(String[] args) {
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("/META-INF/spring/application.xml", DemoApiApplication.class);
 		DemoProperties props = (DemoProperties) context.getBean("appProperties");
-		DemoApiApplication demoApplication = new DemoApiApplication();
-		SpringApplication.run(DemoApiApplication.class, args);
+		//DemoApiApplication demoApplication = new DemoApiApplication();
+		//SpringApplication.run(DemoApiApplication.class, args);
+		SpringApplication sa = new SpringApplication(DemoApiApplication.class);
+		//sa.setApplicationStartup(new BufferingApplicationStartup(2048));
+		sa.setApplicationStartup(new FlightRecorderApplicationStartup());
+		sa.run(args);
 		logger.info("Open this application in your browser at http://localhost:" + props.getRuntimeProperties().getProperty("server.port", ""));
 		context.close();
 	}
